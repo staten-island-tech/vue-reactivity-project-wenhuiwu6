a1f7713f1abe2d0752241b2521559ecb731d72e1
wenhuiwu6 <wenhuiwu678@gmail.com>
2026-02-09 12:32:32 -0500

    111

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,11 +1,12 @@
-<script setup></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div>
+    <h1>Store</h1>
+  </div>
 </template>
 
-<style scoped></style>
+<script setup>
+import { ref } from 'vue'
+const
+</script>
+
+<style lang="scss" scoped></style>
