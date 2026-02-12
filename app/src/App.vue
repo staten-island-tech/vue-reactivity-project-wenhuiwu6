6b3c8857168e3f650c529533527473b80e24eeb3
wenhuiwu6 <wenhuiwu678@gmail.com>
2026-02-12 12:34:33 -0500

    11

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,9 +1,29 @@
 <template>
   <div>
-    <RouterView />
+    <h1>Chinese Restaurant</h1>
+    <p>Eat all you want :></p>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+const foods = ref([
+  { name: 'Tso Chicken', picture: 'KungPaoChicken.jpg', price: 12.99 },
+  { name: 'Kung Pao Chicken', picture: 'TsoChicken.webp', price: 11.99 },
+  { name: 'Sweet and Sour Pork', picture: 'SweetAndSourPork.jpg', price: 10.99 },
+  { name: 'Beef and Broccoli', picture: 'Beef and Broccoli.jpg', price: 13.49 },
+  { name: 'Orange Chicken', picture: 'OrangeChicken.jpg', price: 12.49 },
+  { name: 'Sesame Chicken', picture: 'SesameChicken.jpg', price: 12.99 },
+  { name: 'Chicken Lo Mein', picture: 'ChickenLoMein.jpg', price: 9.99 },
+  { name: 'Shrimp Fried Rice', picture: 'ShrimpFriedRice.jpg', price: 10.49 },
+  { name: 'Vegetable Stir Fry', picture: 'VegetableStirFry.jpg', price: 9.49 },
+  { name: 'Dumplings (Pork)', picture: 'Dumplings.jpg', price: 7.99 },
+  { name: 'Spring Rolls', picture: '', price: 5.99 },
+  { name: 'Hot and Sour Soup', picture: '', price: 4.99 },
+  { name: 'Wonton Soup', picture: '', price: 4.99 },
+  { name: 'Mongolian Beef', picture: '', price: 13.99 },
+  { name: 'Egg Foo Young', picture: '', price: 11.49 },
+])
+</script>
 
 <style lang="scss" scoped></style>
--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,15 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import StorePage from '@views/StorePage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'store',
-      component: StorePage,
-    },
-  ],
+  routes: [],
 })
 
 export default router
