29ff27e902f1361e4610b3da8f6dcc868172910f
wenhuiwu6 <wenhuiwu678@gmail.com>
2026-02-13 12:09:32 -0500

    222

--- app/src/App.vue
+++ app/src/App.vue
@@ -2,12 +2,14 @@
   <div>
     <h1>Chinese Restaurant</h1>
     <p>Eat all you want :></p>
+    <li v-for="food in foods" :key="food.name">{{ food.name }}</li>
+    <li v-for="food in foods" :key="food.name">{{ food.price }}</li>
+    <li v-for="food in foods" :key="food.name">{{ food.picture }}</li>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-const foods = ref([
+const foods = [
   { name: 'Tso Chicken', picture: 'KungPaoChicken.jpg', price: 12.99 },
   { name: 'Kung Pao Chicken', picture: 'TsoChicken.webp', price: 11.99 },
   { name: 'Sweet and Sour Pork', picture: 'SweetAndSourPork.jpg', price: 10.99 },
@@ -18,12 +20,12 @@ const foods = ref([
   { name: 'Shrimp Fried Rice', picture: 'ShrimpFriedRice.jpg', price: 10.49 },
   { name: 'Vegetable Stir Fry', picture: 'VegetableStirFry.jpg', price: 9.49 },
   { name: 'Dumplings (Pork)', picture: 'Dumplings.jpg', price: 7.99 },
-  { name: 'Spring Rolls', picture: '', price: 5.99 },
-  { name: 'Hot and Sour Soup', picture: '', price: 4.99 },
-  { name: 'Wonton Soup', picture: '', price: 4.99 },
-  { name: 'Mongolian Beef', picture: '', price: 13.99 },
-  { name: 'Egg Foo Young', picture: '', price: 11.49 },
-])
+  { name: 'Spring Rolls', picture: 'SpringRolls.jpeg', price: 5.99 },
+  { name: 'Hot and Sour Soup', picture: 'HotAndSourSoup.jpg', price: 4.99 },
+  { name: 'Wonton Soup', picture: 'WontonSoup.jpg', price: 4.99 },
+  { name: 'Mongolian Beef', picture: 'MongolianBeef.jpg', price: 13.99 },
+  { name: 'Egg Foo Young', picture: 'EggFooYoung.jpg', price: 11.49 },
+]
 </script>
 
 <style lang="scss" scoped></style>
