e8ba69304ca36ffc1897e22fcb456dbeafbf2d09
wenhuiwu6 <wenhuiwu678@gmail.com>
2026-02-25 12:38:23 -0500

    NAOOOOOOO

--- app/src/Views/FoodList.vue
+++ app/src/Views/FoodList.vue
@@ -29,4 +29,34 @@ const foods = [
 ]
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+div {
+  padding: 16px;
+}
+
+h1 {
+  text-align: center;
+  margin-bottom: 20px;
+}
+.container {
+  display: flex;
+  width: 80vw;
+  margin: 20px auto;
+  justify-content: space-around;
+  align-items: center;
+  flex-wrap: wrap;
+}
+.card {
+  border: 1px solid #ccc;
+  padding: 12px;
+  margin-bottom: 12px;
+  border-radius: 6px;
+  width: 300px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+}
+</style>
+
+<style lang="" scoped></style>
---
+++ app/src/components/FoodListCards.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h2>{{ food.name }}</h2>
+    <p>{{ food.price }}</p>
+    <img :src="food.img" alt="" />
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  animal: {
+    type: Object,
+    required: true,
+  },
+})
+</script>
+
+<style scoped>
+div {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  height: 300px;
+  width: 200px;
+}
+</style>
